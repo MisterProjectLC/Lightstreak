20e01d1c0a5a15e3ce544ea75e23de6d9320f1d0
MasterProjectLC <dan.inafuku@hotmail.com>
2020-06-04 21:41:20 -0300

    new words

--- Mini Scenes/PhaseManager.gd
+++ Mini Scenes/PhaseManager.gd
@@ -8,6 +8,7 @@ func _ready():
 func load_phase(_phase_id):
 	var file = File.new()
 	if not file.file_exists("res://Files/fase" + str(_phase_id) + ".json"):
+		print("MANO SE VC TA VENDO ESSE ERRO É PQ O GODOT É CORNO, MANDA MENSAGEM PRO DANILO")
 		return
 
 	file.open("res://Files/fase" + str(_phase_id) + ".json", File.READ)
